b9433ea0dae2676e6a28de82dcbd04c24bdc16cb
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-10-20 17:01:57 +0900

    continue react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started! 1q23</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
