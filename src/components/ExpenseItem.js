0eb6027a76c342cf1d67a26dcb01396c1c9e57b2
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-10-24 14:58:17 +0900

    passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,12 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const Temp = new Date();
-  const day = String(Temp.getDate()).padStart(2,0);
-  const month = String(Temp.getMonth()+1).padStart(2,0);
-  const yearNow = Temp.getFullYear();
-
-  const expenseDate = day + '/' + month + '/' + yearNow;
-  const expenseTittle = "Car Insurance";
-  const expenseAmount = 294.67;
-
-
+function ExpenseItem(props) {
   return (
-    <div class = "expense-item">
-      <div>{expenseDate}</div>
-      <div class = "expense-item__description" >
-        <h2>{expenseTittle}</h2>
-        <div class = "expense-item__price" >${expenseAmount}</div>
+    <div class="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div class="expense-item__description">
+        <h2>{props.title}</h2>
+        <div class="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
