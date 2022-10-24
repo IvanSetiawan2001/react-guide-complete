0d684457d8faed80dad7c5bc4c64a544ad3a1ca4
IvanSetiawan2001 <fanset.raharjo@gmail.com>
2022-10-24 11:05:43 +0900

    little change

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,8 +4,8 @@ function ExpenseItem() {
   return (
     <div class = "expense-item">
       <div>March 28th 2021</div>
-      <div >
-        <h2 class = "expense-item__description">Car Insurance</h2>
+      <div class = "expense-item__description" >
+        <h2>Car Insurance</h2>
         <div class = "expense-item__price" >$294.67</div>
       </div>
     </div>
