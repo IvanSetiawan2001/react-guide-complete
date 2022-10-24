99ab22f2a65e8f02cee96497490b88b0b0fe117d
IvanSetiawan2001 <fanset.raharjo@gmail.com>
2022-10-24 12:01:11 +0900

    add dynamic data using javascript

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,22 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const Temp = new Date();
+  const day = String(Temp.getDate()).padStart(2,0);
+  const month = String(Temp.getMonth()+1).padStart(2,0);
+  const yearNow = Temp.getFullYear();
+
+  const expenseDate = day + '/' + month + '/' + yearNow;
+  const expenseTittle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+
   return (
     <div class = "expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate}</div>
       <div class = "expense-item__description" >
-        <h2>Car Insurance</h2>
-        <div class = "expense-item__price" >$294.67</div>
+        <h2>{expenseTittle}</h2>
+        <div class = "expense-item__price" >${expenseAmount}</div>
       </div>
     </div>
   );
