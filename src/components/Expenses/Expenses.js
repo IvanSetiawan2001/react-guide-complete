478b25549246b95542da91994b77c00ed8fdba2a
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-10-26 12:24:47 +0900

    refactor to folder, and change function to arrow function

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (expenses) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={expenses.props[0].title}
+        amount={expenses.props[0].amount}
+        date={expenses.props[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses.props[1].title}
+        amount={expenses.props[1].amount}
+        date={expenses.props[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses.props[2].title}
+        amount={expenses.props[2].amount}
+        date={expenses.props[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses.props[3].title}
+        amount={expenses.props[3].amount}
+        date={expenses.props[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+};
+
+export default Expenses;
