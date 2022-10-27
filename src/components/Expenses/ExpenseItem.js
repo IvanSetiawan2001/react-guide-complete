e937cc4672ba055f0328bd137c0c032642f49957
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-10-27 11:31:52 +0900

    working with event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,20 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  let tittle = props.title;
+
+  const clickHandler = () => {
+    tittle = "Updated";
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2 id="tittle">{tittle}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Tittle</button>
     </Card>
   );
 };
