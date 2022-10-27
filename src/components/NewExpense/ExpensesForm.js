67570ba9efa933c5b79efe4b8c235a2516b69f53
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-10-27 13:19:39 +0900

    correct way to use state that depend on previous state

--- src/components/NewExpense/ExpensesForm.js
+++ src/components/NewExpense/ExpensesForm.js
@@ -14,9 +14,13 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
     });
   };
 
