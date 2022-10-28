3be0250c9df2c95c621e54a54212f73d5fc330c2
Ivan Setiawan <fanset.raharjo@gmail.com>
2022-10-28 11:18:42 +0900

    add expense filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+import ExpensesFilter from "./ExpenseFilter";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
@@ -12,14 +13,17 @@ const ExpenseItem = (props) => {
   };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Tittle</button>
-    </Card>
+    <div>
+      <ExpensesFilter></ExpensesFilter>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Tittle</button>
+      </Card>
+    </div>
   );
 };
 
